11d93cd7a07cbedb6f9c4fc1525f120581b3303b
jiasir <jiasir@icloud.com>
2014-11-12 12:55:24 +0800

    Fix bugs and add options feature

--- god.py
+++ god.py
@@ -8,7 +8,8 @@ import logging
 import ConfigParser
 
 config_file = ConfigParser.RawConfigParser(allow_no_value=True)
-config_file.read('/etc/god/god.conf')
+option_c = sys.argv[2]
+config_file.read('%s' % option_c)
 
 logLevel = config_file.get('logging', 'log_level')
 
@@ -21,13 +22,28 @@ command_line = config_file.get('service', 'command_line')
 delay_loop = config_file.get('global', 'delay_loop')
 delay_before_retry = config_file.get('global', 'delay_before_retry')
 
-while True:
-    time.sleep(int(delay_loop))
-    try:
-        ret = os.popen('ps -C %s -o pid,cmd' % service_name).readlines()
-        if len(ret) < 2:
-            print '%s process error, %s seconds after the restart...' % (service_name, delay_before_retry)
-            time.sleep(int(delay_before_retry))
-            os.system('%s' % command_line)
-    except:
-        print "Error", sys.exc_info()[1]
+def usage():
+    print '''
+    Usage: python god.py -c [config_file]
+    '''
+
+def main():
+    while True:
+        time.sleep(int(delay_loop))
+        try:
+            ret = os.popen('ps -C %s -o pid,cmd' % service_name).readlines()
+            if len(ret) < 2:
+                print '%s process error, %s seconds after the restart...' % (service_name, delay_before_retry)
+                time.sleep(int(delay_before_retry))
+                os.system('%s' % command_line)
+        except:
+            print "Error", sys.exc_info()[1]
+
+
+if __name__ == 'main':
+    if len(sys.argv) == 3 and sys.argv[1] == '-c':
+        main()
+    else:
+        usage()
+
+
