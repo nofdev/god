e0700c5585b48a7ba99422dc5deb692489e8ba99
jiasir <jiasir@icloud.com>
2014-11-12 13:57:09 +0800

    Fix bugs

--- god.py
+++ god.py
@@ -14,7 +14,7 @@ config_file.read('%s' % option_c)
 logLevel = config_file.get('logging', 'log_level')
 
 logger = logging.getLogger('god')
-logging.basicConfig(filename='/var/log/god.log', level=logLevel,
+logging.basicConfig(filename='/var/log/god.log', level=logging.getLevelName(logLevel),
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 service_name = config_file.get('service', 'name')
@@ -22,10 +22,12 @@ command_line = config_file.get('service', 'command_line')
 delay_loop = config_file.get('global', 'delay_loop')
 delay_before_retry = config_file.get('global', 'delay_before_retry')
 
+
 def usage():
     print '''
     Usage: python god.py -c [config_file]
     '''
+
 
 def main():
     while True:
@@ -33,10 +35,11 @@ def main():
         try:
             ret = os.popen('ps -C %s -o pid,cmd' % service_name).readlines()
             if len(ret) < 2:
-                print '%s process error, %s seconds after the restart...' % (service_name, delay_before_retry)
+                logger.error('%s process error, %s seconds after the restart...' % (service_name, delay_before_retry))
                 time.sleep(int(delay_before_retry))
                 os.system('%s' % command_line)
         except:
+            logger.error('Fuck error')
             print "Error", sys.exc_info()[1]
 
 
